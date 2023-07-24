bee27a53f577382164612ce961f8ce40a6bf919f
khairul-bashar <khairulbashar5424@gmail.com>
2023-07-25 01:16:42 +0600

    toy car gallery complagte

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useTitle from '../../Hooks/UseHooks';
+import Gallery from './Gallery/Gallery';
 import Hero from './Hero';
 import ShowAllToy from './ShowAllToy';
 
@@ -8,6 +9,7 @@ const Home = () => {
     return (
         <div>
             <Hero />
+            <Gallery/>
             <ShowAllToy/>
         </div>
     );
