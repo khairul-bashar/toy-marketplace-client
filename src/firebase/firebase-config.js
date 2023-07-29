1b63ab20ae571e0e922ea11141cbdfd70b35f207
khairul-bashar <khairulbashar5424@gmail.com>
2023-07-29 17:53:50 +0600

    firebase config file update

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAt5xL6fouFxeAHFRuCCzZCEGRPWaRsHBg",
-  authDomain: "toy-marketplace-clien-side.firebaseapp.com",
-  projectId: "toy-marketplace-clien-side",
-  storageBucket: "toy-marketplace-clien-side.appspot.com",
-  messagingSenderId: "252209740160",
-  appId: "1:252209740160:web:cac47cc56b57c06330e965",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
